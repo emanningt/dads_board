a3656492e1d39e59b52de80b74715adc6220ffcc
emanningt <emanningmusic@yahoo.com>
2020-04-27 18:58:20 -0700

    updated validations for first_name to last_name
    I had an extrea first_name and it was ment to be last name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     has_many :ideas, dependent: :destroy
 
     validates :first_name,  presence: true
-    validates :first_name,  presence: true
+    validates :last_name,  presence: true
     validates :email, presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true
     has_secure_password
